11fd0a845ef8bdbb4bd338fa444018e01c51fe63
Kiet Vo <tomvo50122002@gmail.com>
2023-11-06 11:28:52 -0600

    Change Emanuel name

--- MasterMind.s
+++ MasterMind.s
@@ -1,6 +1,6 @@
 .global __start
 .data
-    Message: .asciz "Welcome to the Master Mind Game!!!! \nInstructions: \n1: You will have 10 trials to complete the game \n2: In each trial, a player must guess the each color in the correct position and correct color \n3: A player win by having the less guess for the trials \nAuthors: Kiet Vo and Emanual"
+    Message: .asciz "Welcome to the Master Mind Game!!!! \nInstructions: \n1: You will have 10 trials to complete the game \n2: In each trial, a player must guess the each color in the correct position and correct color \n3: A player win by having the less guess for the trials \nAuthors: Kiet Vo and Emanual Cintron"
 
 .text
 __start:
