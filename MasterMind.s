fa0a457a4b65efc8c66c74b5c1aef29edaa0df61
Kiet Vo <tomvo50122002@gmail.com>
2023-11-05 22:23:49 -0600

    Print the message for the game

--- MasterMind.s
+++ MasterMind.s
@@ -1,1 +1,14 @@
-sadfasdf
+.global __start
+.data
+    Message: .asciz "Welcome to the Master Mind Game!!!! \nInstructions: \n1: You will have 10 trials to complete the game \n2: In each trial, a player must guess the each color in the correct position and correct color \n3: A player win by having the less guess for the trials \nAuthors: Kiet Vo and Emanual"
+
+.text
+__start:
+    #Message on 
+    addi a0, x0, 4
+    la a1, Message
+    ecall
+
+    # Exit program 
+    addi a0, x0, 10
+    ecall
